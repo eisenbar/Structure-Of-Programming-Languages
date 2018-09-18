ba2ec0655c8c3f3c605d572c0622daab11b8e971
Ryan Eisenbarth <ryan.eisenbarth@gmail.com>
2018-09-18 15:52:09 -0400

    updated reverse to full functionality

--- Project1/reverse.c
+++ Project1/reverse.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include "file_utils.c"
 #include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
 
-char* input = argv[1]
-char* output = argv[2]
+	if( arc != 3){
+		fprintf( stderr, "Incorrect inputs");
+		return errno;
+	}
 
-struct stat st;
-stat(*argv[1], &st);
-int inputSize = st.st_size
+	char* input = argv[1];
+	char* output = argv[2];
 
-read_file(input, inputSize)
+	struct stat st;
+	stat(*argv[1], &st);
+	int inputSize = st.st_size;
 
-return 0
+	char* buffer = (char*)malloc(sizeof(char)*inputSize);
+	char* reverse = (char*)malloc(sizeof(char)*inputSize);
+
+	read_file(input, &buffer);
+
+	for(int i = 0; i >=0; i--){
+		reverse[i] = buffer[inputSize - i - 1];
+	}
+
+	write_file(output, reverse, inputSize);
+
+
+	free(buffer);
+	free(reverse);
+	return 0;
 }
